45834b3556733554a4135f5e22e31bf344e3f9c3
nataliafalek <faleknatalia@gmail.com>
2016-11-27 17:56:34 +0100

    dzialajaca gierka

--- src/main/scala/pl/nfalek/game2048.scala
+++ src/main/scala/pl/nfalek/game2048.scala
@@ -1,78 +1,65 @@
 package pl.nfalek
 
 object game2048 {
-  def main(args: Array[String]) {
-//    println(shiftRight2(List(2, 2, 4, 8)) == List(0, 4, 4, 8))
-//    println(shiftRight2(List(2, 4, 4, 2)) == List(0, 2, 8, 2))
-//    println(shiftRight2(List(2, 4, 2, 2)) == List(0, 2, 4, 4))
-//    println(shiftRight2(List(8, 4, 4, 2)) == List(0, 8, 8, 2))
-//    println(shiftRight2(List(0, 4, 4, 2)) == List(0, 0, 8, 2))
-//    println(shiftRight2(List(0, 4, 4, 0)) == List(0, 0, 0, 8))
-//    println(shiftRight2(List(4, 4, 0, 0)) == List(0, 0, 0, 8), shiftRight2(List(4, 4, 0, 0)))
-//    println(shiftRight2(List(0, 0, 2, 2)) == List(0, 0, 0, 4))
-//    println(shiftRight2(List(0, 2, 2, 2)) == List(0, 0, 2, 4))
-//    println(shiftRight2(List(4, 2, 2, 2)) == List(0, 4, 2, 4))
-//    println(shiftRight2(List(2, 2, 2, 4)) == List(0, 2, 4, 4))
-//    println(shiftRight2(List(2, 2, 2, 0)) == List(0, 0, 2, 4))
-//    println(shiftRight2(List(2, 2, 2, 2)) == List(0, 0, 4, 4))
-//        println(matrix.shiftMatrixRight())
-//        println(matrix.shiftMatrixDown())
-//        println(matrix.shiftMatrixLeft())
-//        println(matrix.shiftMatrixUp())
-//        println(matrix.insertFieldRandomly())
-//        println(Board.initializeBoard())
+    def main(args: Array[String]): Unit = {
+      play_game(Board.initializeBoard())
+     // println(Console.BLUE + "2")
+    // val colors_map = Map (
+    //   0 -> (Console.YELLOW + "0"),
+    //   2 -> (Console.BLUE + "2"),
+    //   4 -> (Console.RED + "4")
+     //)
+      //println(List(0, 2, 4).map(aa => colors_map(aa)), List(2, 2, 4).map(aa => colors_map(aa)))
+      //println(colors_map(0))
+    }
 
-  }
+    def play_game(b: Board): Board = {
+     // val new_board = Board.initializeBoard()
+      println(b)
+      val line = scala.io.StdIn.readLine()
+      val fucking_Board = if (line == "a") {
+        play_game(b.shiftMatrixLeft().play)
+      } else if (line == "d") {
+        play_game(b.shiftMatrixRight().play)
+      } else if (line == "w") {
+        play_game(b.shiftMatrixUp().play)
+      } else if (line == "s") {
+        play_game(b.shiftMatrixDown().play)
+      } else {
+       play_game(b)
+      }
+      println(fucking_Board)
+     fucking_Board
+    }
 
-  val matrix = Board(List(
-    List(0,0,2,0),
-    List(2,2,2,2),
-    List(0,0,2,4),
-    List(0,0,0,0)
-  ))
-
-//
-//  def shiftRight2(row: List[Int]): List[Int] = {
-//
-//    val a0 = row(0)
-//    val a1 = row(1)
-//    val a2 = row(2)
-//    val a3 = row(3)
-//
-//    if (a0 == a1 && a1 == a2 && a2 == a3) {
-//      List(0, 0, a0 + a1, a2 + a3)
-//    } else if (a0 == a1 && a2 == a3 && a0 != a2 && a2 != 0) {
-//      List(0, 0, a0 + a1, a2 + a3)
-//    } else if (a0 == a1 && a2 == a3 && a0 != a2 && a2 == 0) {
-//      List(0, 0, 0, a0 + a1)
-//    } else if (a0 == a1 && a2!=a1 && a2 != a3 && a3 != 0) {
-//      List(0, a0 + a1, a2, a3)
-//    } else if (a0 == a1 && a1 == a2 && a2 != a3 && a3 == 0) {
-//      List(0, 0, a0 + a1, a2)
-//    } else if (a0 == 0 && a1 == a2 && a2 == a3) {
-//      List(0, 0, a1, a2 + a3)
-//    } else if (a0 != 0 && a1 == a2 && a2 == a3) {
-//      List(0, a0, a1, a2 + a3)
-//    } else if (a0 == a3 && a1 == a2 && a1 == 0) {
-//      List(0, 0, 0, a0 + a3)
-//    } else if (a1 == a2 && a1 != 0 && a3 != 0) {
-//      List(0, a0, a1 + a2, a3)
-//    } else if (a3 == 0 && a0 != a3 && a1 == a2) {
-//      List(0, 0, a0, a1 + a2)
-//    } else if (a3 == 0 && a0 == a3 && a1 == a2) {
-//      List(0, 0, 0, a1 + a2)
-//    } else if (a0 == a1 && a1 == a2 && a2 != a3) {
-//      List(0, a0, a1 + a2, a3)
-//    } else if (a0 == a1 && a1 == a3 & a0 == 0) {
-//      List(0, 0, 0, a2)
-//    } else if (a0!=a1 && a2==a3){
-//      List(0, a0, a1, a2 + a3)
-//    } else {
-//      row
-//    }
-//
-//  }
   case class Board(rows: List[List[Int]]) {
+    override def toString(): String = {
+      val colors_map = Map (
+        0 -> Console.YELLOW,
+        2 -> Console.BLUE,
+        4 -> Console.RED,
+        8 -> Console.MAGENTA,
+        16 -> Console.CYAN,
+        32 -> Console.WHITE,
+        64 -> Console.BLINK,
+        128 -> Console.GREEN
+      ).withDefaultValue("")
+      rows.map(row => row.map(aa => colors_map(aa) + aa).mkString("\t")).mkString("\n")
+      //println(colors_map(2))
+      //println(colors_map(4))
+      //aa.map(bb => colors_map(bb))
+    }
+
+    def play: Board = {
+       if (hasAnySpaceLeft()) {
+         Board(rows).insertFieldRandomly()
+       } else {
+         throw new GameOverException
+       }
+ //       throw new GameOverException
+ //     }
+//      throw new GameOverException
+    }
 
     def shiftZero(row: List[Int]): List[Int] = {
       val listWithoutZero = row.filter(aa => aa!=0)
@@ -149,6 +136,10 @@ object game2048 {
       Board(rows.updated(random_column, rows(random_column).updated(random_row, random_number)))
 
     }
+
+    def hasAnySpaceLeft(): Boolean = {
+      rows.flatten.filter(aa => aa == 0).size > 0
+    }
   }
 
   object Board {
@@ -158,4 +149,5 @@ object game2048 {
     }
   }
 
+  class GameOverException extends Exception
 }
